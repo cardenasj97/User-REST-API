a570cb68c0d0947c18747abcd366c79c516c4435
Juan Cardenas <cardenas19890@gmail.com>
2021-01-20 07:32:20 -0500

    permission feature added

---
+++ src/common/middlewares/auth.permission.middleware.js
@@ -0,0 +1,27 @@
+exports.minimumPermissionLevelRequired = (required_permission_level) => {
+    return (req, res, next) => {
+        let user_permission_level = parseInt(req.jwt.permissionLevel);
+
+        if (user_permission_level & required_permission_level) {
+            return next();
+        } else {
+            return res.status(403).send('Insufficient permissions.');
+        }
+    };
+};
+
+exports.onlySameUserOrAdminCanDoThisAction = (req, res, next) => {
+    let user_permission_level = parseInt(req.jwt.permissionLevel);
+    let userId = req.jwt.userId;
+    
+    if (req.params && req.params.userId && userId === req.params.userId) {
+        return next();
+    } else {
+        if (user_permission_level & ADMIN_PERMISSION) {
+            return next();
+        } else {
+            return res.status(403).send();
+        }
+    }
+
+};
--- src/users/routes.config.js
+++ src/users/routes.config.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const UsersController = require('./controllers/users.controller');
+const PermissionMiddleware = require('./../common/middlewares/auth.permission.middleware');
+const ValidationMiddleware = require('./../common/middlewares/auth.validation.middleware');
+const config = require('./../common/config/env.config');
+const ADMIN = config.permissionLevels.ADMIN;
+const PAID = config.permissionLevels.PAID_USER;
+const FREE = config.permissionLevels.NORMAL_USER;
+
 const router = new express.Router();
 
 router.post('/users', [
     UsersController.insert
 ]);
 
+router.get('/users', [
+    ValidationMiddleware.validJWTNeeded,
+    PermissionMiddleware.minimumPermissionLevelRequired(PAID),
+    UsersController.list
+]);
+
 router.get('/users/:userId', [
+    ValidationMiddleware.validJWTNeeded,
+    PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+    PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
     UsersController.getById
 ]);
 
 router.patch('/users/:userId', [
+    ValidationMiddleware.validJWTNeeded,
+    PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+    PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
     UsersController.patchById
 ]);
 
-router.get('/users', [
-    UsersController.list
-]);
-
 router.delete('/users/:userId', [
+    ValidationMiddleware.validJWTNeeded,
+    PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
     UsersController.removeById
 ]);
 
